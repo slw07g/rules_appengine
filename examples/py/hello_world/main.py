d04b1d662cce94c330f1bac9e599eff4a6ebec6f
Shanief Webb <slw07g@users.noreply.github.com>
2023-08-15 11:32:02 -0400

    Update import

--- examples/py/hello_world/main.py
+++ examples/py/hello_world/main.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import webapp2
+import webapp3 as webapp2
 
 
 class MainPage(webapp2.RequestHandler):
